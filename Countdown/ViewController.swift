7b4b47d9240468abaac5582932219127b3b4c8e1
Mintesnot Shigutie <mintesnotshigutie@Mintesnotui-MacBookPro.local>
2022-01-20 19:06:47 -0500

    start

---
+++ Countdown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Mintesnot Shigutie on 2022/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
